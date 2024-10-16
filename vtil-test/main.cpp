33ae399d4336fdab6edaaac39b7995bdffac7a24
helloobaby <sun888486@qq.com>
2024-10-16 20:44:44 +0800

    Update

--- vtil-test/main.cpp
+++ vtil-test/main.cpp
@@ -1,33 +1,37 @@
-/*
-将下面这段代码lift到vtil
-
-LREGDW 0x68
-LREGQ 0x58
-LCONSTQ 0x140003640
-ADDQ
-SREGQ 0x70
-WRITEDW
-
-
-
-
-
-*/
-
-
 #include<vtil/vtil>
+static constexpr vtil::register_desc make_virtual_register(uint8_t context_offset, uint8_t size)
+{
+	fassert(((context_offset & 7) + size) <= 8 && size);
 
+	return {
+		vtil::register_virtual,
+		(size_t)context_offset / 8,
+		size * 8,
+		(context_offset % 8) * 8
+	};
+}
 int main()
 {
 	auto block = vtil::basic_block::begin(0x14000AC83);
-	vtil::register_desc reg(vtil::register_virtual, 64, 32, (0x68%8)*8);
-	vtil::register_desc reg2(vtil::register_virtual, 64, 64, (0x58 % 8) * 8);
-	block->push(reg);
-	block->push(reg2);
-	block->push(0x140003640);
-	
 
-	vtil::debug::dump(block->owner);
+	// mov qword ptr [rax+0x140003640], 1337
+	block->mov(make_virtual_register(0x58, 8), X86_REG_RAX);
+	block->push(make_virtual_register(0x58, 8));
+	block->push(vtil::operand(0x140003640,64));
+	auto [o1, o2] = block->tmp(64, 64);
+	block->pop(o2);
+	block->pop(o1);
+	block->add(o1, o2); // ignore eflags
+	block->push(o1);
+	block->pop(make_virtual_register(0x70, 8));
+	block->str(make_virtual_register(0x70, 8), 0, 1337);
+	// 
+
+	vtil::debug::dump(block);
+
+	vtil::optimizer::apply_all(block);
+
+	vtil::debug::dump(block);
 
 	return 0;
 }
